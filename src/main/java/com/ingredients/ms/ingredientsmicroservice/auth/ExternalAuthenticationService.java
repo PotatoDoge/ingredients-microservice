63d0488d8003569ddd9e0f9b046d7213946bab98
bprado <bastian.prado.v@gmail.com>
2024-01-27 13:29:29 -0800

    Add external authentication service

---
+++ src/main/java/com/ingredients/ms/ingredientsmicroservice/auth/ExternalAuthenticationService.java
@@ -0,0 +1,45 @@
+package com.ingredients.ms.ingredientsmicroservice.auth;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class ExternalAuthenticationService {
+
+    private final String externalAuthUrl;
+    private final RestTemplate restTemplate;
+
+    public ExternalAuthenticationService(@Value("${external.auth.url}") String externalAuthUrl, RestTemplate restTemplate) {
+        this.externalAuthUrl = externalAuthUrl;
+        this.restTemplate = restTemplate;
+    }
+
+    public boolean isAuthenticated(String authToken) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", authToken);
+        int responseStatusCode = 0;
+
+        try{
+            HttpEntity<String> entity = new HttpEntity<>(headers);
+
+
+            ResponseEntity<String> response = restTemplate.exchange(
+                    externalAuthUrl,
+                    HttpMethod.GET,
+                    entity,
+                    String.class
+            );
+
+            responseStatusCode = response.getStatusCode().value();
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        return responseStatusCode == 200;
+    }
+}
+
+
